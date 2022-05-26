5b96b20a60f7c4661dc45217f98ab2c314dc390c
guiraigumi <63981480+guiraigumi@users.noreply.github.com>
2022-05-26 12:31:29 +0200

    David_Alcaide_Examen

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //funcion para cargar el primer nivel
+    public void LoadLVL1()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+}
